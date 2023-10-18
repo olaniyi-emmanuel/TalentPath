4d0a1b7eb9fc4b79c39892313c5911cf63b06b1b
olaniyi-emmanuel <emmanuel4sucess@outlook.com>
2023-10-18 22:52:21 +0100

    </xs:annotation>

---
+++ src/main/java/com/olaniyi/HibernateSpeakerRepositoryImpl.java
@@ -0,0 +1,14 @@
+package com.olaniyi;
+
+import com.olaniyi.model.Speaker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HibernateSpeakerRepositoryImpl {
+
+    public List<Speaker> findall () {
+        List<Speaker> speakers = new ArrayList<Speaker>()
+
+    }
+}
